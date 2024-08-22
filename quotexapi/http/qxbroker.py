47b95bf79946757c43eb575f6a418a8f0bc40c9c
cleiton <cleiton.leonel@gmail.com>
2024-08-22 13:24:10 -0300

    :boom: fix: Revertendo mudanças ineficientes.

--- quotexapi/http/qxbroker.py
+++ quotexapi/http/qxbroker.py
@@ -88,6 +88,7 @@ class Browser(object):
         else:
             browser = await playwright.firefox.launch(
                 headless=True,
+                args=self.args,
             )
             context = await browser.new_context(
                 viewport={
