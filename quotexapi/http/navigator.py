e6c63e5fb11fa28c91ef20d73328aa96fb36869e
cleiton <cleiton.leonel@gmail.com>
2025-03-31 16:07:32 -0300

    :zap: Perf: substituir Playwright por requests e otimizar requisições.

--- quotexapi/http/navigator.py
+++ quotexapi/http/navigator.py
@@ -1,9 +1,13 @@
-import random
-import requests
+import ssl
+from requests import Session
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from bs4 import BeautifulSoup
-from ..http.user_agents import agents
+
+user_agent_list = [
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) "
+    "Gecko/20100101 Firefox/116.0",
+]
 
 retry_strategy = Retry(
     total=3,
@@ -11,22 +15,109 @@ retry_strategy = Retry(
     status_forcelist=[429, 500, 502, 503, 504, 104],
     allowed_methods=["HEAD", "POST", "PUT", "GET", "OPTIONS"]
 )
-adapter = HTTPAdapter(max_retries=retry_strategy)
-user_agent_list = agents.split("\n")
 
 
-class Browser(object):
-    response = None
-    headers = None
+class CipherSuiteAdapter(HTTPAdapter):
+    __attrs__ = [
+        'ssl_context',
+        'max_retries',
+        'config',
+        '_pool_connections',
+        '_pool_maxsize',
+        '_pool_block',
+        'source_address'
+    ]
 
-    def __init__(self):
-        self.session = requests.Session()
-        self.session.mount("https://", adapter)
-        self.session.mount("http://", adapter)
+    def __init__(self, *args, **kwargs):
+        self.ssl_context = kwargs.pop('ssl_context', None)
+        self.cipherSuite = kwargs.pop('cipherSuite', None)
+        self.source_address = kwargs.pop('source_address', None)
+        self.server_hostname = kwargs.pop('server_hostname', None)
+        self.ecdhCurve = kwargs.pop('ecdhCurve', 'prime256v1')
+
+        if self.source_address:
+            if isinstance(self.source_address, str):
+                self.source_address = (self.source_address, 0)
+
+            if not isinstance(self.source_address, tuple):
+                raise TypeError(
+                    "source_address must be IP address string "
+                    "or (ip, port) tuple"
+                )
+
+        if not self.ssl_context:
+            self.ssl_context = ssl.create_default_context(
+                ssl.Purpose.SERVER_AUTH
+            )
+
+            self.ssl_context.orig_wrap_socket = self.ssl_context.wrap_socket
+            self.ssl_context.wrap_socket = self.wrap_socket
+
+            if self.server_hostname:
+                self.ssl_context.server_hostname = self.server_hostname
+
+            self.ssl_context.set_ciphers(self.cipherSuite)
+            self.ssl_context.set_ecdh_curve(self.ecdhCurve)
+
+            self.ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
+            self.ssl_context.maximum_version = ssl.TLSVersion.TLSv1_3
+
+        super(CipherSuiteAdapter, self).__init__(**kwargs)
+
+    def wrap_socket(self, *args, **kwargs):
+        if (hasattr(self.ssl_context, 'server_hostname')
+                and self.ssl_context.server_hostname):
+            kwargs['server_hostname'] = self.ssl_context.server_hostname
+            self.ssl_context.check_hostname = False
+        else:
+            self.ssl_context.check_hostname = True
+
+        return self.ssl_context.orig_wrap_socket(*args, **kwargs)
+
+    def init_poolmanager(self, *args, **kwargs):
+        kwargs['ssl_context'] = self.ssl_context
+        kwargs['source_address'] = self.source_address
+        return super(CipherSuiteAdapter, self).init_poolmanager(
+            *args, **kwargs
+        )
+
+    def proxy_manager_for(self, *args, **kwargs):
+        kwargs['ssl_context'] = self.ssl_context
+        kwargs['source_address'] = self.source_address
+        return super(CipherSuiteAdapter, self).proxy_manager_for(
+            *args, **kwargs
+        )
+
+
+class Browser(Session):
+
+    def __init__(self, *args, **kwargs):
+        self.response = None
+        self.headers = self.get_headers()
+        self.ecdhCurve = kwargs.pop('ecdhCurve', 'prime256v1')
+        self.cipherSuite = kwargs.pop('cipherSuite', 'DEFAULT@SECLEVEL=1')
+        self.source_address = kwargs.pop('source_address', None)
+        self.server_hostname = kwargs.pop('server_hostname', None)
+        self.ssl_context = kwargs.pop('ssl_context', None)
+
+        super(Browser, self).__init__()
+
+        self.mount(
+            'https://',
+            CipherSuiteAdapter(
+                ecdhCurve=self.ecdhCurve,
+                cipherSuite=self.cipherSuite,
+                server_hostname=self.server_hostname,
+                source_address=self.source_address,
+                ssl_context=self.ssl_context,
+                max_retries=retry_strategy
+            )
+        )
 
     def set_headers(self, headers=None):
         self.headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
                           "Chrome/87.0.4280.88 Safari/537.36"
         }
         if headers:
@@ -34,11 +125,27 @@ class Browser(object):
                 self.headers[key] = value
 
     def get_headers(self):
+        self.headers = {
+            "User-Agent": user_agent_list[0],
+        }
         return self.headers
 
+    def get_cookies(self):
+        return '; '.join(
+            ['%s=%s' % (i.name, i.value) for i in self.cookies]
+        )
+
     def get_soup(self):
-        return BeautifulSoup(self.response.content, "html.parser")
+        return BeautifulSoup(
+            self.response.content,
+            "html.parser"
+        )
 
     def send_request(self, method, url, **kwargs):
-        self.response = self.session.request(method, url, headers=self.headers, **kwargs)
+        self.response = self.request(
+            method,
+            url,
+            headers=self.headers,
+            **kwargs
+        )
         return self.response
