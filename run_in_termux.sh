7e54acd2c9ffc8d25c5c4c4b79c1319bde23396a
cleiton <cleiton.leonel@gmail.com>
2025-04-01 12:22:23 -0300

    :package: feat: add installation script for Termux (Android).

---
+++ run_in_termux.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+CURRENT_DIR=$(basename "$PWD")
+
+pkg update && upgrade -y
+
+pkg install \
+  git \
+  build-essential \
+  cmake \
+  ninja \
+  libopenblas \
+  libandroid-execinfo \
+  patchelf \
+  binutils-is-llvm \
+  openssl \
+  python-numpy -y
+
+if [ "$CURRENT_DIR" = "pyquotex" ]; then
+    echo "You're already in the pyquotex directory."
+else
+    git clone https://github.com/cleitonleonel/pyquotex.git -o pyquotex
+    cd pyquotex || exit
+fi
+
+pip install -U pip
+pip install poetry
+
+poetry install
+
+NUMPY_INFO=$(pip show numpy)
+
+SITE_PACKAGES_PATH=$(poetry run python -c "import site; print(site.getsitepackages()[0])")
+cp -r "${NUMPY_INFO}/numpy" "${SITE_PACKAGES_PATH}"
+
+poetry run python app.py get_profile
