5f5b479a5e2728d08709122379a7ac7c30d9d97f
cleiton <cleiton.leonel@gmail.com>
2025-04-07 12:07:34 -0300

    :bug: fix: Fix pip update installation in termux.

--- run_in_termux.sh
+++ run_in_termux.sh
@@ -25,15 +25,18 @@ if [ "$CURRENT_DIR" = "pyquotex" ]; then
 else
     git clone https://github.com/cleitonleonel/pyquotex.git -o pyquotex
     cd pyquotex || exit
+    git checkout e68433c
+
 fi
 
 pip install poetry
 
 poetry install
 
-NUMPY_INFO=$(pip show numpy)
+echo "[*] Copy NumPy into the Poetry environment..."
+NUMPY_PATH=$(python3 -c "import numpy; import os; print(os.path.dirname(numpy.__file__))")
 
 SITE_PACKAGES_PATH=$(poetry run python -c "import site; print(site.getsitepackages()[0])")
-cp -r "${NUMPY_INFO}/numpy" "${SITE_PACKAGES_PATH}"
+cp -r "${NUMPY_PATH}" "${SITE_PACKAGES_PATH}"
 
 poetry run python app.py get_profile
